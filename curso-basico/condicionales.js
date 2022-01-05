dbc975a12e6c92655bb41b2add81acfd6abcc17c
Pablo Calderon salazar <pablocalderonsalazar@MacBook-Air-de-Pablo.local>
2022-01-05 16:49:35 -0500

    Class 12 - Conditional challenge resolved

--- curso-basico/condicionales.js
+++ curso-basico/condicionales.js
@@ -21,3 +21,45 @@ function ageVerification(edad){
 }
 
 ageVerification(18)
+
+// Piedra, papel o tijera
+
+var choices = ["piedra", "papel", "tijera"]
+var cpu_choice;
+var my_choice;
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var choices_dic = {
+    0: "Piedra",
+    1: "Papel",
+    2: "Tijera"
+};
+
+cpu_choice = choices_dic[randomInteger(0,2)];
+my_choice = choices_dic[Number(prompt("Ingrese: 0 para piedra, 1 para papel, 2 para tijera "))]
+
+console.log("La CPU escogió " + cpu_choice);
+console.log(my_choice);
+
+function piedraPapelTijera2(cpu_choice,my_choice){
+    if (cpu_choice == my_choice){
+        return "It is a tie."
+    } else if((cpu_choice==choices_dic[0])&&(my_choice==choices_dic[1])){
+        return "You won."
+    } else if((cpu_choice==choices_dic[0])&&(my_choice==choices_dic[2])){
+        return "Computer won."
+    } else if((cpu_choice==choices_dic[1])&&(my_choice==choices_dic[0])){
+        return "Computer won."
+    } else if((cpu_choice==choices_dic[1])&&(my_choice==choices_dic[2])){
+        return "You won."
+    } else if((cpu_choice==choices_dic[2])&&(my_choice==choices_dic[0])){
+        return "You won."
+    } else if((cpu_choice==choices_dic[2])&&(my_choice==choices_dic[1])){
+        return "Computer won."
+    }
+}
+
+piedraPapelTijera2(cpu_choice,my_choice)
