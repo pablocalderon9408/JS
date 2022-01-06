db5fb6f6638c9911b1273852a783ecf04b820592
Pablo Calderon salazar <pablocalderonsalazar@MacBook-Air-de-Pablo.local>
2022-01-06 07:47:39 -0500

    Class 15-16: Arrays and loops

---
+++ curso-basico/arrays.js
@@ -0,0 +1,9 @@
+var frutas = ["Manzana", "Banano", "Fresa"];
+
+function saludarFruta(fruta){
+    console.log(`Hola, ${fruta}`);
+}
+
+for (var i = 0 ; i < frutas.length; i++){
+    saludarFruta(frutas[i])
+}
