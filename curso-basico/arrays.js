0557b21c04efae382091e3b93a1c83b5c002a0c4
Pablo Calderon salazar <pablocalderonsalazar@MacBook-Air-de-Pablo.local>
2022-01-06 07:49:08 -0500

    Class 14-15-16: Arrays and loops(for and while)

--- curso-basico/arrays.js
+++ curso-basico/arrays.js
@@ -1,9 +1,25 @@
+
 var frutas = ["Manzana", "Banano", "Fresa"];
 
 function saludarFruta(fruta){
     console.log(`Hola, ${fruta}`);
 }
 
+// opción con while
+while(frutas.length>0){
+    var fruta = frutas.pop()
+    saludarFruta(fruta)
+
+}
+
+// opción 1 con for
 for (var i = 0 ; i < frutas.length; i++){
     saludarFruta(frutas[i])
 }
+
+// opción 2 con for
+for (var fruta of frutas){
+    saludarFruta(fruta)
+}
+
+
