e75ed51899490fae7b905311c895cc54d36e7c21
Pablo Calderon <pablocalderon9408@gmail.com>
2022-05-04 17:36:40 -0500

    create function to calculate square area

--- curso-practico/figuras.js
+++ curso-practico/figuras.js
@@ -1,15 +1,36 @@
 console.group("Cuadrado");
+// const ladoCuadrado = 10;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado);
 
-const ladoCuadrado = 10;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+function perimetroCuadrado(lado){
+    return lado * 4;
+}
 
-const perimetroCuadrado = ladoCuadrado * 4;
 console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
+function areaCuadrado(lado){
+    return lado * lado;
+}
+
 console.log("El area del cuadrado es: " + areaCuadrado);
 
-const pi = Math.PI;
-console.log("El valor de pi es: " + pi);
+// const pi = Math.PI;
+// console.log("El valor de pi es: " + pi);
 
 console.groupEnd();
+
+
+// Interacción con el html:
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(`El perimetro del cuadrado es: ${perimetro}`);
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(`El area del cuadrado es: ${area}`);
+}
